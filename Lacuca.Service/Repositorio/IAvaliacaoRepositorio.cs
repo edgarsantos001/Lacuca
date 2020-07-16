bfa92dd91fc687935551769b9b8b97094a2ecb31
Edgar A. Santos <edgar_19031@hotmail.com>
2020-07-16 17:26:45 -0300

    Adicionando Interface

---
+++ Lacuca.Service/Repositorio/IAvaliacaoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Lacuca.Service.DataBase.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lacuca.Service.Repositorio
+{
+  interface IAvaliacaoRepositorio:IRepository<AvaliacaoModel>
+  {
+
+  }
+}
